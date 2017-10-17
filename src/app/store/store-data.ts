5944fb4caa7fd09dee137859364f09411febaf2c
peter palocz <peter.palocz91@gmail.com>
2017-10-17 16:38:34 +0200

    delete files

--- src/app/store/store-data.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from '../../../shared/model/message';
-import { Thread } from '../../../shared/model/thread';
-import { Participant } from '../../../shared/model/participant';
-
-export interface StoreData {
-  participants: { [key: number]: Participant };
-  threads: { [key: number]: Thread };
-  messages: { [key: number]: Message };
-}
--- src/app/store/ui-state.ts
+++
@@ -1,4 +0,0 @@
-export interface UiState {
-  userId: number;
-  currentThreadId: number;
-}
