d29ebc0084aa62e4c5d71d44ffb7d92686e9329a
peter palocz <peter.palocz91@gmail.com>
2017-10-17 10:37:38 +0200

    feat: add thread&message sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserSelectionComponent
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent
   ],
   imports: [
     BrowserModule
