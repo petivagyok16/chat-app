068e7713e4ead6325138db22a13f30c135693037
peter palocz <peter.palocz91@gmail.com>
2017-10-17 17:14:27 +0200

    feat: add reducer

--- src/app/store/reducers/store-data-state.reducer.ts
+++ src/app/store/reducers/store-data-state.reducer.ts
@@ -1,6 +1,19 @@
+import { ActionTypes, LoadUserThreadsAction } from '../actions';
 import { Action } from '@ngrx/store';
 import { StoreDataState } from '../states/store-data-state';
+import * as _ from 'lodash';
 
-export function storeDataStateReducer(storeDataState: StoreDataState, action: Action): StoreDataState {
-  return storeDataState;
+export function storeDataStateReducer(storeDataState: StoreDataState, action: LoadUserThreadsAction): StoreDataState {
+  switch(action.type) {
+    case ActionTypes.LOAD_USER_THREADS_ACTION:
+      return {
+        ...storeDataState,
+        participants: _.keyBy(action.payload.participants, 'id'),
+        threads: _.keyBy(action.payload.threads, 'id'),
+        messages: _.keyBy(action.payload.messages, 'id'),
+      }
+
+    default:
+      return storeDataState;
+  }
 }
