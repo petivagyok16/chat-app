a51c504a4cac12355af7294746a4d8059fd22417
peter palocz <peter.palocz91@gmail.com>
2017-10-18 14:59:02 +0200

    feat: add debugger to Observable

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,36 @@ import { environment } from './environments/environment';
 
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Rx';
+
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    debug: (...any) => Observable<T>
+  }
+};
+
+const debuggerOn = true;
+
+Observable.prototype.debug = function(message: string) {
+  return this.do(
+    nextValue => {
+      if (debuggerOn) {
+        console.log(message, nextValue);
+      }
+    },
+    error => {
+      if (debuggerOn) {
+        console.log(message, error);
+      }
+    },
+    () => {
+      if (debuggerOn) {
+        console.log("Observable completed - ", message);
+      }
+    }
+  )
+};
 
 if (environment.production) {
   enableProdMode();
