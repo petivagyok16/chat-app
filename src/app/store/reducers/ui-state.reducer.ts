71c733dbf8d0322da52f7a5a7599166d15647ee0
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:01:34 +0200

    feat: add ui-state reducer

--- src/app/store/reducers/ui-state.reducer.ts
+++ src/app/store/reducers/ui-state.reducer.ts
@@ -1,6 +1,12 @@
 import { Action } from '@ngrx/store';
-import { UiState } from '../states/ui-state';
+import { INITIAL_UI_STATE, UiState } from '../states/ui-state';
 
-export function uiStateReducer(uiState: UiState, action: Action): UiState {
-  return uiState;
+export function uiStateReducer(uiState: UiState = INITIAL_UI_STATE, action: Action): UiState {
+  switch(action.type) {
+
+    
+
+    default:
+    return uiState;
+  }
 }
