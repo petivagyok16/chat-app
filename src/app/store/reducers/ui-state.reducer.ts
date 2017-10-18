9ecef1811ac3787bc7178c7e22098854cc1d5dee
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:18:53 +0200

    refactor: refactor ui-state reducer

--- src/app/store/reducers/ui-state.reducer.ts
+++ src/app/store/reducers/ui-state.reducer.ts
@@ -1,12 +1,20 @@
 import { Action } from '@ngrx/store';
 import { INITIAL_UI_STATE, UiState } from '../states/ui-state';
+import { ActionTypes, ThreadSelectedAction } from '../actions';
 
 export function uiStateReducer(uiState: UiState = INITIAL_UI_STATE, action: Action): UiState {
-  switch(action.type) {
-
-    
+  switch (action.type) {
+    case ActionTypes.THREAD_SELECTED_ACTION:
+      return handleThreadSelectedAction(uiState, <ThreadSelectedAction>action);
 
     default:
     return uiState;
+  }
+}
+
+function handleThreadSelectedAction(uiState: UiState, action: ThreadSelectedAction) {
+  return {
+    ...uiState,
+    currentThreadId: action.payload
   }
 }
