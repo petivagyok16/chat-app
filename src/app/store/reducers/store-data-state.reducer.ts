2562b98ae00406b9a0016ced8222f2fba92d47f4
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:01:02 +0200

    refactor: refactor action name

--- src/app/store/reducers/store-data-state.reducer.ts
+++ src/app/store/reducers/store-data-state.reducer.ts
@@ -1,11 +1,11 @@
-import { ActionTypes, LoadUserThreadsAction } from '../actions';
+import { ActionTypes, UserThreadsLoadedAction } from '../actions';
 import { Action } from '@ngrx/store';
 import { StoreDataState } from '../states/store-data-state';
 import * as _ from 'lodash';
 
-export function storeDataStateReducer(storeDataState: StoreDataState, action: LoadUserThreadsAction): StoreDataState {
+export function storeDataStateReducer(storeDataState: StoreDataState, action: UserThreadsLoadedAction): StoreDataState {
   switch(action.type) {
-    case ActionTypes.LOAD_USER_THREADS_ACTION:
+    case ActionTypes.USER_THREADS_LOADED_ACTION:
       return {
         ...storeDataState,
         participants: _.keyBy(action.payload.participants, 'id'),
