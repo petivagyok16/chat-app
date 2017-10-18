1aacf5c0b9834bf34bc08367a463c575fea2edca
peter palocz <peter.palocz91@gmail.com>
2017-10-18 10:25:53 +0200

    refactor: delete import

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
 
 import { AllUserData } from '../../../shared/to/all-user-data';
 
