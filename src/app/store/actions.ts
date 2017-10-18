12cecfba238d3eb57edb14bfc5a5f3775f62c038
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:00:31 +0200

    feat: add action

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -3,11 +3,22 @@ import { type } from '../utils/action-type-check';
 import { AllUserData } from '../../../shared/to/all-user-data';
 
 export const ActionTypes = {
+  USER_THREADS_LOADED_ACTION: type("[Threads] User threads loaded"),
   LOAD_USER_THREADS_ACTION: type("[Threads] Load user threads"),
+  THREAD_SELECTED_ACTION: type("[Thread] A thread was selected.")
 };
 
 export class LoadUserThreadsAction implements Action {
-  readonly type: string = ActionTypes.LOAD_USER_THREADS_ACTION;
+  readonly type = ActionTypes.LOAD_USER_THREADS_ACTION;
+}
+
+export class UserThreadsLoadedAction implements Action {
+  readonly type: string = ActionTypes.USER_THREADS_LOADED_ACTION;
   constructor(public payload: AllUserData) { }
   
+}
+
+export class ThreadSelectedAction implements Action {
+  readonly type: string = ActionTypes.THREAD_SELECTED_ACTION;
+  constructor(public payload: number) { }
 }
