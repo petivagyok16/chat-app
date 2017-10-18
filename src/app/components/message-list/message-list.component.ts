d70acf18e269cac527b826c20c0d95bf640bead2
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:50:29 +0200

    feat: add message-list cmp

--- src/app/components/message-list/message-list.component.ts
+++ src/app/components/message-list/message-list.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  ElementRef,
+  ViewChild,
+  SimpleChanges
+} from '@angular/core';
+import * as _ from 'lodash';
+
+import { MessageVM } from '../../../../shared/model/message'
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnChanges {
+  @Input() messages: MessageVM[];
+  @ViewChild('list') list: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['messages']) {
+      const previousMessages = changes['messages'].previousValue;
+      const newMessages = changes['messages'].currentValue;
+      if (previousMessages && newMessages.length > previousMessages.length) {
+        setTimeout(() => {
+          this.scrollLastMessageIntoView();
+        });
+      }
+    }
+  }
+
+  scrollLastMessageIntoView() {
+    const items = this.list.nativeElement.querySelectorAll('li');
+    const lastItem: any = _.last(items);
+    if (lastItem) {
+      lastItem.scrollIntoView();
+    }
+  }
 }
