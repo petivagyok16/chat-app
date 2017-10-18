c4296ed3dddaaa2b8623acb7731c6849ff3e23dc
peter palocz <peter.palocz91@gmail.com>
2017-10-18 15:00:08 +0200

    feat: implement thread-section cmp

--- src/app/components/thread-section/thread-section.component.ts
+++ src/app/components/thread-section/thread-section.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
 
 import { ApplicationState } from '../../store/states/application-state';
-import { LoadUserThreadsAction } from '../../store/actions';
+import { LoadUserThreadsAction, ThreadSelectedAction } from '../../store/actions';
 import { ThreadSummary } from './thread-summary.vm';
 
 @Component({
@@ -20,45 +20,52 @@ export class ThreadSectionComponent implements OnInit {
   public threadSummaries$: Observable<ThreadSummary[]>;
 
   constructor(
-    private threadsService: ThreadsService,
     private store: Store<ApplicationState>
   ) {
-    this.userName$ = this.store
-      .skip(1)
-      .map(this.mapStateToUserName);
-    
-    this.unreadMessagesCounter$ = this.store
-      .skip(1)
-      .map(this.mapStateToUnreadMessagesCounter);
-
-    this.threadSummaries$ = store.select(state => {
-      const threads = _.values<Thread>(state.storeDataState.threads);
-      return threads.map(thread => {
-        
-        const names = _.keys(thread.participants).map(participantId => state.storeDataState.participants[participantId].name);
-
-        return { 
-          id: thread.id,
-          participantNames: null,
-          lastMessageText: null,
-        }
-      });
-    })
+    this.store.subscribe(console.log);
+    this.userName$ = this.store.select(this.userNameSelector);
+    this.unreadMessagesCounter$ = this.store.map(this.mapStateToUnreadMessagesCounter);
+    this.threadSummaries$ = store.select(this.mapStateToThreadSummaries);
   }
 
-  private mapStateToUserName(state: ApplicationState): string {
-    return state.storeDataState.participants[state.uiState.userId].name;
+  private mapStateToThreadSummaries(state: ApplicationState): ThreadSummary[] {
+    const threads = _.values<Thread>(state.storeDataState.threads);
+    return threads.map(thread => {
+      const names = Object.keys(thread.participants).map(participantId => state.storeDataState.participants[participantId].name);
+      const lastMessageId = _.last(thread.messageIds);
+
+      return { 
+        id: thread.id,
+        participantNames: _.join(names, ", "),
+        lastMessageText: state.storeDataState.messages[lastMessageId].text,
+        timestamp: state.storeDataState.messages[lastMessageId].timestamp,
+      }
+    });
+  }
+
+  private userNameSelector(state: ApplicationState): string {
+    const currentUserId = state.uiState.userId;
+    const currentParticipant = state.storeDataState.participants[currentUserId];
+
+    if (!currentParticipant) {
+      return "";
+    }
+
+    return currentParticipant.name;
   }
 
   private mapStateToUnreadMessagesCounter(state: ApplicationState): number {
     const currentUserId = state.uiState.userId;
     return _.values<Thread>(state.storeDataState.threads)
-      .reduce((acc, thread) => acc + thread.participants[currentUserId], 0)
+      .reduce((acc, thread) => acc + (thread.participants[currentUserId] || 0), 0)
   }
 
   ngOnInit() {
-    this.threadsService.loadUserThreads()
-      .subscribe(allUserData => this.store.dispatch(new LoadUserThreadsAction(allUserData)));
+    this.store.dispatch(new LoadUserThreadsAction())
+  }
+
+  public onThreadSelected(threadId: number) {
+    this.store.dispatch(new ThreadSelectedAction(threadId))
   }
 
 }
