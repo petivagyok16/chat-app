a893877727525ee00e61d29aeb40b3d6e41c4ad7
peter palocz <peter.palocz91@gmail.com>
2017-10-19 09:08:40 +0200

    feat: add select user action

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -5,11 +5,13 @@ import { AllUserData } from '../../../shared/to/all-user-data';
 export const ActionTypes = {
   USER_THREADS_LOADED_ACTION: type("[Threads] User threads loaded"),
   LOAD_USER_THREADS_ACTION: type("[Threads] Load user threads"),
-  THREAD_SELECTED_ACTION: type("[Thread] A thread was selected.")
+  THREAD_SELECTED_ACTION: type("[Thread] A thread was selected."),
+  SELECT_USER_ACTION: type("[User] New user was selected.")
 };
 
 export class LoadUserThreadsAction implements Action {
   readonly type = ActionTypes.LOAD_USER_THREADS_ACTION;
+  constructor(public payload: number) { }
 }
 
 export class UserThreadsLoadedAction implements Action {
@@ -20,5 +22,10 @@ export class UserThreadsLoadedAction implements Action {
 
 export class ThreadSelectedAction implements Action {
   readonly type: string = ActionTypes.THREAD_SELECTED_ACTION;
+  constructor(public payload: number) { }
+}
+
+export class SelectUserAction implements Action {
+  readonly type: string = ActionTypes.SELECT_USER_ACTION;
   constructor(public payload: number) { }
 }
