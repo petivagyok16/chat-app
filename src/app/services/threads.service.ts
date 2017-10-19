142e6b8ba81cbdd577f43d1b2a30f9d7b3b4079f
peter palocz <peter.palocz91@gmail.com>
2017-10-19 13:35:05 +0200

    feat: add requests

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
+import { commonHttpHeaders } from './shared/commonHttpHeaders';
 
 import { AllUserData } from '../../../shared/to/all-user-data';
+import { SendNewMessageAction } from '../store/actions';
+import { Message } from './../../../shared/model/message';
 
 @Injectable()
 export class ThreadsService {
@@ -10,11 +13,33 @@ export class ThreadsService {
   constructor(private http: Http) { }
 
   loadUserThreads(userId: number): Observable<AllUserData> {
-    const headers = new Headers();
-    headers.append('userid', userId.toString());
+    if (!userId) {
+      return;
+    }
 
-    return this.http.get('/api/threads', { headers })
-      .map(res => res.json());
+    return this.http.get('/api/threads', commonHttpHeaders(userId))
+      .map(res => <AllUserData>res.json());
   }
+
+  saveNewMessage(payload: { text: string, threadId: number, participantId: number }): Observable<any> {
+    return this.http.post(`/api/threads/${payload.threadId}`,
+      JSON.stringify({text: payload.text}),
+      commonHttpHeaders(payload.participantId)
+    );
+  }
+
+  loadNewMessagesForUser(userId: number): Observable<Message[]> {
+    return this.http.post('/api/notifications/messages', null, commonHttpHeaders(userId))
+      .map(res => res.json().payload);
+    // return Observable.of(new Array<Message>());
+  }
+
+  // markMessagesAsRead(payload: ThreadSelectedActionPayload): Observable<any> {
+  //   // Using PATCH since we are taking on action on the thread, and not creating (POST) or updating (PUT)
+  //   return this.http.patch(`/api/threads/${payload.selectedThreadId}`,
+  //     JSON.stringify({read: true}),
+  //     commonHttpHeaders(payload.currentUserId)
+  //   );
+  // }
 
 }
