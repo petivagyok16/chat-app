0c24076c69bf214b5516d7ca1274fa221c1c6bb6
peter palocz <peter.palocz91@gmail.com>
2017-10-19 14:12:15 +0200

    feat: add catch operator

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
 import { Observable } from 'rxjs/Rx';
 
