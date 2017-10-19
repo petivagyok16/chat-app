df762c9968bb0cf7a9fc0e99a539519de291d04c
peter palocz <peter.palocz91@gmail.com>
2017-10-19 09:09:21 +0200

    feat: handle select user action

--- src/app/store/reducers/ui-state.reducer.ts
+++ src/app/store/reducers/ui-state.reducer.ts
@@ -1,11 +1,14 @@
 import { Action } from '@ngrx/store';
 import { INITIAL_UI_STATE, UiState } from '../states/ui-state';
-import { ActionTypes, ThreadSelectedAction } from '../actions';
+import { ActionTypes, SelectUserAction, ThreadSelectedAction } from '../actions';
 
 export function uiStateReducer(uiState: UiState = INITIAL_UI_STATE, action: Action): UiState {
   switch (action.type) {
     case ActionTypes.THREAD_SELECTED_ACTION:
       return handleThreadSelectedAction(uiState, <ThreadSelectedAction>action);
+
+    case ActionTypes.SELECT_USER_ACTION:
+      return handleSelectUserAction(uiState, <SelectUserAction>action);
 
     default:
       return uiState;
@@ -16,5 +19,12 @@ function handleThreadSelectedAction(uiState: UiState, action: ThreadSelectedActi
   return {
     ...uiState,
     currentThreadId: action.payload
+  }
+}
+
+function handleSelectUserAction(uiState: UiState, action: SelectUserAction) {
+  return {
+    ...uiState,
+    userId: action.payload
   }
 }
