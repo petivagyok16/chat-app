9927b9307c01639b79391eb42ab8604c4eed8bd0
peter palocz <peter.palocz91@gmail.com>
2017-10-19 09:08:50 +0200

    feat: add user selected effect

--- src/app/store/effects/load-threads-effect.service.ts
+++ src/app/store/effects/load-threads-effect.service.ts
@@ -1,24 +1,25 @@
 import { Observable } from 'rxjs/Rx';
 import { Action } from '@ngrx/store';
-import { ActionTypes, LoadUserThreadsAction, UserThreadsLoadedAction } from '../actions';
+import { ActionTypes, LoadUserThreadsAction, SelectUserAction, UserThreadsLoadedAction } from '../actions';
 import { ThreadsService } from '../../services/threads.service';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 
 @Injectable()
 export class LoadThreadsEffectService {
-  @Effect() userThreads$: Observable<Action> = this.actions$
-    .ofType(ActionTypes.LOAD_USER_THREADS_ACTION)
-    .debug(val => {console.log('Action received -> ', val)})
-    .switchMap(() => this.threadsService.loadUserThreads())
-    .debug(data => {console.log('data arrived-> ', data )})
-    .map(allUserData => new UserThreadsLoadedAction(allUserData));
-
   constructor(
     private threadsService: ThreadsService,
     private actions$: Actions
-  ) {
+  ) { }
 
-  }
+  @Effect() userThreads$: Observable<Action> = this.actions$
+  .ofType(ActionTypes.LOAD_USER_THREADS_ACTION)
+  .debug(val => {console.log('Action received -> ', val)})
+  .switchMap((action: LoadUserThreadsAction) => this.threadsService.loadUserThreads(action.payload))
+  .debug(data => {console.log('data arrived-> ', data )})
+  .map(allUserData => new UserThreadsLoadedAction(allUserData));
 
+  @Effect() newUserSelected$: Observable<Action> = this.actions$
+  .ofType(ActionTypes.SELECT_USER_ACTION)
+  .map((action: SelectUserAction) => new LoadUserThreadsAction(action.payload))
 }
