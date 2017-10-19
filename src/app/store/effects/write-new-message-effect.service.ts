b7a56156e1117073dde30775ccfd824e9b54abbb
peter palocz <peter.palocz91@gmail.com>
2017-10-19 13:35:21 +0200

    feat: write new message

--- src/app/store/effects/write-new-message-effect.service.ts
+++ src/app/store/effects/write-new-message-effect.service.ts
@@ -1,10 +1,18 @@
+import { ActionTypes } from '../actions';
+import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 import { ThreadsService } from '../../services/threads.service';
-import { Actions } from '@ngrx/effects';
+import { Actions, Effect } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { SendNewMessageAction } from '../actions'
 
 @Injectable()
 export class WriteNewMessageEffectService {
 
-  constructor() { }
+  constructor(private actions$: Actions, private threadsService: ThreadsService) { }
 
+  @Effect({ dispatch: false })
+    newMessages$: Observable<any> = this.actions$
+      .ofType(ActionTypes.SEND_NEW_MESSAGE_ACTION)
+      .switchMap((action: SendNewMessageAction) => this.threadsService.saveNewMessage(action.payload))
 }
